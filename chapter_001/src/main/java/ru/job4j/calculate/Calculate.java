24946b762611081f79011584a94b4943946fbfdb
Nikolay_Ilin <salence1309@gmail.com>
2018-08-17 23:36:50 +0900

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,30 @@
 package ru.job4j.calculate;
 
+/**
+*Calculate.
+*
+*@author Nikolay Ilin (salence1309@gmail.com)
+*@version $Id$
+*since 0.1
+*/
+
 public class Calculate {
 	
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+
+
+/**
+* Method echo
+* @param name Nikolay Ilin.
+* @return Echo plus Nikolay Ilin.
+*/
+	public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Nikolay Ilin (mailto:salence1309@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
