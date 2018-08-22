bd8617933cd5e3490424d156bd2b9d27335c15d4
Nikolay_Ilin <salence1309@gmail.com>
2018-08-22 15:48:37 +0900

    2.2. Импорт Maven проекта. [#31168]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,23 +5,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Nikolay Ilin (mailto:salence1309@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Nikolay Ilin (mailto:salence1309@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Nikolay Ilin";
-    String expect = "Echo, echo, echo : Nikolay Ilin"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Nikolay Ilin";
+        String expect = "Echo, echo, echo : Nikolay Ilin";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
