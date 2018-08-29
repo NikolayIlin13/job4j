194a3b0fc66a29e11c70c8bd573178d6dabac2da
Nikolay_Ilin <salence1309@gmail.com>
2018-08-29 23:06:37 +0900

    3.2. Конвертер валюты. [#41583]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,20 +1,24 @@
 package ru.job4j.calculator;
-public class Calculator{
+public class Calculator {
     private double result;
+
     public void add(double first, double second) {
         this.result = first + second;
     }
-    public void subtract (double first, double second) {
+
+    public void subtract(double first, double second) {
         this.result = first - second;
     }
-    public void div (double first, double second) {
+
+    public void div(double first, double second) {
         this.result = first / second;
     }
-    public void  multipl (double first, double second) {
+
+    public void multipl(double first, double second) {
         this.result = first * second;
     }
+
     public double getResult() {
         return this.result;
     }
-
 }
