ba9f4d50aef6e7440866dea53f108b6736b21e25
Nikolay_Ilin <salence1309@gmail.com>
2018-09-11 05:50:06 +0900

    4.1. Глупый бот. [#31323]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -2,15 +2,17 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class PointTest {
 
     @Test
-    public void distanceTo() {
-    }
-
-    @Test
-    public void main() {
+    public void testDistanceFromOnePointToAnother() {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+        double actual = a.distanceTo(b);
+        double expected = Math.sqrt(20);
+        assertThat(actual, is(expected));
     }
 }
